cd6e6dc60d58502cb45abb7d4bcb60779c96636b
wangxinyi <wangxinyi@wangxinyideMacBook-Air.local>
2014-10-08 21:35:42 +0800

    Initial Commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by wangxinyi on 14-10-8.
+//  Copyright (c) 2014年 sdzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by wangxinyi on 14-10-8.
+//  Copyright (c) 2014年 sdzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
